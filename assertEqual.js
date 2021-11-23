0cfef76b0cd24fc8ebe11f0bbf1c053259109dbe
Fadime Serap Keskinler <fsekurt@gmail.com>
2021-11-23 15:20:10 -0500

    Initial commit

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  return console.log("Assertion Passed: " + actual + " === " + expected);
+  } else return console.log("Assertion Failed: " + actual + " !=== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,3);
