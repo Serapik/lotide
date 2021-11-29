cffea53bd5a8b829a8fb63594218ecc76d00d7da
Fadime Serap Keskinler <fsekurt@gmail.com>
2021-11-28 21:24:25 -0500

    Initial Commit

---
+++ takeUntil.js
@@ -0,0 +1,9 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let i = 0; !callback(array[i]) && i < array.length; i++) {
+    results.push(array[i]);
+  }
+  return results;
+};
+
+module.exports = takeUntil;
