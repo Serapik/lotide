97ad9f40d62f6da9c8eb6f0d4dcc635b51a25f62
Fadime Serap Keskinler <fsekurt@gmail.com>
2021-11-29 23:30:57 -0500

    Test folder

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+// const assertEqual = function(actual, expected) {
+//   // ...
+// }
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,3);
+
+
+module.exports = assertEqual;
